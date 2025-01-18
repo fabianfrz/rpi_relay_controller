69f3ee882e753a2472bf2e0cdded0df19264ecd9
Fabian Franz BSc <franz.fabian.94@gmail.com>
2025-01-18 16:25:50 +0100

    add more relay functionality

--- relays.c
+++ relays.c
@@ -1,14 +1,60 @@
 #include "mongoose.h"
 #include <wiringPi.h>
 #include <systemd/sd-daemon.h>
+#include <stdio.h>
+#include <argon2.h>
+#include <math.h>
 
 char* headers_json = "Content-Type: application/json\r\n";
 char* format_error = "{\"ok\": false, \"error\": \"%s\"}";
 
-unsigned int relayOutputCount = 1;
-int relayOutputs[] = {17};
+unsigned int relayOutputCount = 4;
+int relayOutputs[] = {17, 18, 27, 22};
 
+struct user {
+  const char *name, *pass;
+};
 
+struct usernode {
+    struct usernode* next;
+    struct user* user;
+};
+
+struct usernode* users;
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+// Parse HTTP requests, return authenticated user or NULL
+static struct user *getuser(struct mg_http_message *hm) {
+  char user[256], pass[256];
+  struct usernode *u;
+  mg_http_creds(hm, user, sizeof(user), pass, sizeof(pass));
+  if (user[0] != '\0' && pass[0] != '\0') {
+    u = users;
+    while (u != NULL) {
+      if (strcmp(user, u->user->name) == 0 && strcmp(pass, u->user->pass) == 0) {
+        return u->user;
+      }
+    }
+  }
+  return NULL;
+}
+
+/*
+bool verifyHash(char* password, char* hash) {
+    if (hash.rfind("$argon2id") == 0) {
+        return argon2id_verify(hash.c_str(), password.c_str(), password.size()) == ARGON2_OK;
+    } else if (hash.rfind("$argon2i") == 0) {
+        return argon2i_verify(hash.c_str(), password.c_str(), password.size()) == ARGON2_OK;
+    } else if (hash.rfind("$argon2d") == 0) {
+        return argon2d_verify(hash.c_str(), password.c_str(), password.size()) == ARGON2_OK;
+    } else {
+        return false; // the hash is not valid
+    }
+}
+*/
 void ev_get_set_relays(struct mg_connection *c, struct mg_http_message *hm, struct mg_str* caps) {
 	int relay_id;
 	if (!mg_str_to_num(caps[0], 10, &relay_id, sizeof(relay_id))) {
@@ -79,11 +125,45 @@ void prepareWPI() {
     wiringPiSetupGpio();
     for (int i = 0; i < relayOutputCount; i++) {
         pinMode(relayOutputs[i], OUTPUT);
+	digitalWrite(relayOutputs[i], LOW);
     }
 }
 
+void readUserDatabase() {
+    FILE* file = fopen("users.csv", "r");
+    if (file == NULL) {
+        printf("User database users.csv is missing\n");
+        exit(1);
+    }
+    char user[256];
+    char password[256];
+    char buf[1024];
+    struct usernode* lastNode = NULL;
+    while (fscanf(file, "%1023s", buf) == 1) {
+        char* passwordOffset = strchr(buf, ':');
+	if (passwordOffset == NULL) {
+            printf("Line is not readable: %s", buf);
+	    exit(1);
+	}
+	strncpy(user, buf, min(passwordOffset - buf, sizeof(user) - 1));
+	strncpy(password, passwordOffset + 1, sizeof(password) - 1);
+        struct usernode* un = malloc(sizeof(struct usernode));
+	un->user = calloc(1, sizeof(user));
+	if (lastNode != NULL) {
+            un->next = lastNode;
+	} else {
+            users = un;
+	}
+        lastNode = un;
+	un->user->name = strdup(user);
+	un->user->pass = strdup(password);
+    }
+    fclose(file);
+    users = lastNode;
+}
 
 int main(void) {
+    readUserDatabase();
     prepareWPI();
     struct mg_mgr mgr;
     mg_mgr_init(&mgr);
@@ -92,6 +172,6 @@ int main(void) {
     for (;;) {
         mg_mgr_poll(&mgr, 1000);
     }
-  return 0;
+    return 0;
 }
 
